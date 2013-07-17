96734cc52c6ce2534572f354c7a3a46d39791ca6
tukiyo3 <tukiyo3@gmail.com>
2013-07-17 22:23:12 +0900

    add:package install, mutt

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -7,7 +7,7 @@ ADD_BASHRC=`grep .bashrc_mine $HOME/.bashrc`
 for file in .*
 do
     case $file in
-    "."|".."|".git"|".gitignore"|".set_dotfiles.sh.swp")
+    "."|".."|".git"|".gitignore"|".set_dotfiles.sh.swp"|".muttrc")
         continue
         ;;
     esac
@@ -21,19 +21,33 @@ do
         mkdir -p "$BACKUPDIR"
       fi
       echo "[info] $file exists."
-      echo "[info] mv $HOME/$file $BACKUPDIR"
+      echo "[exec] mv $HOME/$file $BACKUPDIR"
       mv $HOME/$file "$BACKUPDIR"
     fi
 
-    echo "[info] ln -s `pwd`/$file $HOME/$file"
+    echo "[exec] ln -s `pwd`/$file $HOME/$file"
     ln -s `pwd`/$file $HOME/$file
+    echo
 done
 
 mkdir -p ${HOME}/local/{bin,src,program,newsbeuter-saved-articles}
+mkdir -p ${HOME}/.mutt/cache/
+if [ ! -e $HOME/.muttrc ];then
+    echo "[exec] cp .muttrc $HOME/.muttrc"
+    cp .muttrc $HOME/.muttrc
+fi
 
 if [ "$ADD_BASHRC" = "" ];then
     echo "[info] source `pwd`/.bashrc_mine >> $HOME/.bashrc"
     echo source '$HOME/.bashrc_mine' >> $HOME/.bashrc
 else
-    echo "[info] already wrote .bashrc_mine"
+    echo "[skip] already wrote .bashrc_mine"
+fi
+
+if [ -e /etc/debian_version ]; then
+    echo "[info] apt-get install"
+    sudo apt-get install \
+       ranger atool mediainfo transmission-cli poppler-utils highlight caca-utils w3m \
+       mutt-patched mailutils \
+       vim git tig openssh-server openssh-client
 fi

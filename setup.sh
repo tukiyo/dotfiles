680432657ca01ce64ecbb62035d4b9f588cd549c
tukiyo3 <tukiyo3@gmail.com>
2013-07-23 11:55:51 +0900

    add:mac configurations.

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+BACKUPDIR=$HOME/backup/
+
+#-------------------------------------------------------------------------------
+for file in .*
+do
+    case $file in
+    "."|".."|".git"|".gitignore"|".set_dotfiles.sh.swp"|".muttrc")
+        continue
+        ;;
+    esac
+
+    if [ -e $HOME/$file ]; then
+      if [ -L $HOME/$file ]; then
+        echo "[skip] already $file exists."
+        continue
+      fi
+      if [ ! -e $BACKUPDIR ]; then
+        mkdir -p "$BACKUPDIR"
+      fi
+      echo "[info] $file exists."
+      echo "[exec] mv $HOME/$file $BACKUPDIR"
+      mv $HOME/$file "$BACKUPDIR"
+    fi
+
+    echo "[exec] ln -s `pwd`/$file $HOME/$file"
+    ln -s `pwd`/$file $HOME/$file
+    echo
+done
